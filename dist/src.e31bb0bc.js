25da1257116f69d544e8ea6eaa4393fc506d25f9
Marcos García F <marcos@iMac-de-Marcos.local>
2020-04-29 21:07:03 +0200

    Build

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -154,7 +154,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51369" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50645" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
